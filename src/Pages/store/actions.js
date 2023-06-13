d437ddf0b6e6efa0bf23c4a87d2b916cbf48036e
Diksha <dikshaomsingh@gmail.com>
2023-06-14 01:16:41 +0530

    Updates

--- src/Pages/store/actions.js
+++
@@ -1,5 +0,0 @@
-// Authentication module
-export * from "./auth/register/actions";
-export * from "./auth/login/actions";
-export * from "./auth/forgetpwd/actions";
-export * from "./auth/profile/actions";
